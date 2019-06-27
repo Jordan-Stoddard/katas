bb142ed308478c5d8810a6c43134db1ebfe6c462
Jordan Stoddard <jjsincorporated@gmail.com>
2019-06-27 16:43:55 -0700

    save pennies variable

--- Medium/convenienceStore.js
+++ Medium/convenienceStore.js
@@ -24,7 +24,7 @@ function changeEnough(change, amountDue) {
     const quarters = (change[0] * 25 * .01)
     const dimes = (change[1] * 10 * .01)
     const nickels = (change[2] * 5 * .01)
-    // const pennies = (change[3] * 1 * .01)
+    const pennies = (change[3] * 1 * .01)
     // const amountHave = (quarters + dimes + nickels + pennies)
 
     // if (amountHave < amountDue) {

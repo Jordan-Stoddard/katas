d9d632ad6535ae84ba018580bb4a18291dab2bdc
Jordan Stoddard <jjsincorporated@gmail.com>
2019-06-28 09:12:49 -0700

    add pseudocode

--- Medium/factorizeANumber.js
+++ Medium/factorizeANumber.js
@@ -10,6 +10,12 @@
 // The input integer will be positive.
 // A factor is a number that evenly divides into another number without leaving a remainder. The second example is a factor of 12, because 12 / 2 = 6, with remainder 0.
 
+// Loop num times.
+// test if num is divisible by i,
+// if it is, push it into the array
+// if not continue
+// Once you've reached the end of the loop, return the array.
+
 function factorize(num) {
 	let arr = []
 	for (let i = 1; i <= num; i++) {

796a110fe0416bed3e6ca7f62f66a3d4716e770c
Jordan Stoddard <jjsincorporated@gmail.com>
2019-06-28 08:29:52 -0700

    initialize challenge

---
+++ Medium/checkEquals.js
@@ -0,0 +1,11 @@
+function checkEquals(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}

30d541c945dfd87c6b0e56829e3f92c9610a35a1
Jordan Stoddard <jjsincorporated@gmail.com>
2019-08-05 14:33:32 -0700

    initialize problem

---
+++ CodeSignal/checkPalindrome.js
@@ -0,0 +1,23 @@
+// For inputString = "aabaa", the output should be
+// checkPalindrome(inputString) = true;
+// For inputString = "abac", the output should be
+// checkPalindrome(inputString) = false;
+// For inputString = "a", the output should be
+// checkPalindrome(inputString) = true.
+
+
+
+
+
+function checkPalindrome(inputString) {
+    return inputString.reverse() === inputString;
+}
+
+console.log(checkPalindrome('aabaa')) // true
+console.log(checkPalindrome('abac')) // false
+console.log(checkPalindrome('a')) // true
+console.log(checkPalindrome('az')) // false
+console.log(checkPalindrome('abacaba')) // true
+
+
+

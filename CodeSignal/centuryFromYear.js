f2ca37030ae321987140464436975fd5c4812b1c
Jordan Stoddard <jjsincorporated@gmail.com>
2019-08-05 14:26:12 -0700

    complete problem

--- CodeSignal/centuryFromYear.js
+++ CodeSignal/centuryFromYear.js
@@ -1,5 +1,5 @@
-// Given a year, return the century it is in. 
-// The first century spans from the year 1 up to and including the year 100, the second - 
+// Given a year, return the century it is in.
+// The first century spans from the year 1 up to and including the year 100, the second -
 // from the year 101 up to and including the year 200, etc.
 
 // Example
@@ -20,62 +20,66 @@
 // if turnOfCentury is true, return +concatenated string
 // if turnOfCentury is false, return +concatenated string + 1
 
-
 function centuryFromYear(year) {
-    const iterableYear = year.toString()
-    let turnOfCentury = false
-    let century = ''
-    if (year <= 99) {
-        return 1
-    }
-    let onesPlace = iterableYear[iterableYear.length - 1]
-    let tensPlace = iterableYear[iterableYear.length - 2]
-    if (onesPlace === '0' && tensPlace === '0') {
-        turnOfCentury = true
-    }
+  const iterableYear = year.toString();
+  let turnOfCentury = false;
+  let century = "";
+  if (year <= 99) {
+    return 1;
+  }
+  let onesPlace = iterableYear[iterableYear.length - 1];
+  let tensPlace = iterableYear[iterableYear.length - 2];
+  if (onesPlace === "0" && tensPlace === "0") {
+    turnOfCentury = true;
+  }
 
-    for (let i = 0; i < iterableYear.length; i++) {
-        let currentNumber = iterableYear[i]
-        if (i >= 2) {
-            continue
-        } else {
-            century += currentNumber
-        }
-    }
-
-    if (turnOfCentury) {
-        return +century
+  for (let i = 0; i < iterableYear.length; i++) {
+    let currentNumber = iterableYear[i];
+    if (iterableYear.length === 3) {
+      if (i > 0) {
+        continue;
+      } else {
+        century += currentNumber;
+      }
     } else {
-        return +century + 1
+      if (i > 1) {
+        continue;
+      } else {
+        century += currentNumber;
+      }
     }
+  }
 
+  if (turnOfCentury) {
+    return +century;
+  } else {
+    return +century + 1;
+  }
 }
-
 
 // Input:
 // year: 8
 // Expected Output:
 // 1
-console.log(centuryFromYear(8))
+console.log(centuryFromYear(8));
 // Input:
 // year: 45
 // Expected Output:
 // 1
-console.log(centuryFromYear(45))
+console.log(centuryFromYear(45));
 
 // Input:
 // year: 374
 // Expected Output:
 // 4
 
-console.log(centuryFromYear(374))
+console.log(centuryFromYear(374));
 
 // Input:
 // year: 2001
 // Expected Output:
 // 21
 
-console.log(centuryFromYear(2001))
-console.log(centuryFromYear(1988))
-console.log(centuryFromYear(2000))
-
+console.log(centuryFromYear(2001));
+console.log(centuryFromYear(1988));
+console.log(centuryFromYear(2000));

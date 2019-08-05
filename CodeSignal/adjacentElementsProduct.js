984cb1b8cb8f33d079b560b3579b69eb2dc592c9
Jordan Stoddard <jjsincorporated@gmail.com>
2019-08-05 14:37:10 -0700

    intitialize problem

---
+++ CodeSignal/adjacentElementsProduct.js
@@ -0,0 +1,21 @@
+// Given an array of integers, find the pair of adjacent elements
+// that has the largest product and return that product.
+
+// Example
+
+// For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+// adjacentElementsProduct(inputArray) = 21.
+
+// 7 and 3 produce the largest product.
+
+function adjacentElementsProduct(inputArray) {
+
+}
+
+console.log(-1 * -2)
+
+console.log(adjacentElementsProduct([-1, -2])) // 2
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3])) // 21
+console.log(adjacentElementsProduct([5, 1, 2, 3, 1, 4])) // 6
+console.log(adjacentElementsProduct([5, 6, -4, 2, 3, 2, -23])) // 30
+

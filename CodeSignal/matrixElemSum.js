86b0cd1bd90df08a63021a8606c567da7ed4e5e8
Jordan Stoddard <jjsincorporated@gmail.com>
2019-08-08 14:53:00 -0700

    initialize tests

--- CodeSignal/matrixElemSum.js
+++ CodeSignal/matrixElemSum.js
@@ -1,24 +1,28 @@
-// After becoming famous, the CodeBots decided to move into a new building together. 
-// Each of the rooms has a different cost, and some of them are free, 
-// but there's a rumour that all the free rooms are haunted! Since the CodeBots are quite superstitious, 
+// After becoming famous, the CodeBots decided to move into a new building together.
+// Each of the rooms has a different cost, and some of them are free,
+// but there's a rumour that all the free rooms are haunted! Since the CodeBots are quite superstitious,
 // they refuse to stay in any of the free rooms, or any of the rooms below any of the free rooms.
 
-// Given matrix, a rectangular matrix of integers, 
-// where each value represents the cost of the room, 
-// your task is to return the total sum of all rooms that are suitable for the CodeBots 
+// Given matrix, a rectangular matrix of integers,
+// where each value represents the cost of the room,
+// your task is to return the total sum of all rooms that are suitable for the CodeBots
 // (ie: add up all the values that don't appear below a 0).
 
 // Example
 
 // For
 
-// matrix = [[0, 1, 1, 2], 
-//           [0, 5, 0, 0], 
+// matrix = [[0, 1, 1, 2],
+//           [0, 5, 0, 0],
 //           [2, 0, 3, 3]]
 
 // the output should be
 // matrixElementsSum(matrix) = 9.
 
-function matrixElementsSum(matrix) {
+function matrixElementsSum(matrix) {}
 
-}
+console.log(matrixElementsSum([[0, 1, 1, 2], [0, 5, 0, 0], [2, 0, 3, 3]])); // 9
+console.log(matrixElementsSum([[1, 1, 1, 0], [0, 5, 0, 1], [2, 1, 3, 10]])); // 9
+console.log(matrixElementsSum([[1, 1, 1], [2, 2, 2], [3, 3, 3]])); // 18
+console.log(matrixElementsSum([[0]])); // 0
+console.log(matrixElementsSum([[1, 0, 3], [0, 2, 1], [1, 2, 0]])); // 5

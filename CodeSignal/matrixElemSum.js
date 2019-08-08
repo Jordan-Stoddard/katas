8cc30c614195aaf6384998e1fd135116c01d7306
Jordan Stoddard <jjsincorporated@gmail.com>
2019-08-08 15:53:54 -0700

    problem complete

--- CodeSignal/matrixElemSum.js
+++ CodeSignal/matrixElemSum.js
@@ -68,7 +68,7 @@ function matrixElementsSum(matrix) {
 }
 
 console.log(matrixElementsSum([[0, 1, 1, 2], [0, 5, 0, 0], [2, 0, 3, 3]])); // 9
-// console.log(matrixElementsSum([[1, 1, 1, 0], [0, 5, 0, 1], [2, 1, 3, 10]])); // 9
-// console.log(matrixElementsSum([[1, 1, 1], [2, 2, 2], [3, 3, 3]])); // 18
-// console.log(matrixElementsSum([[0]])); // 0
-// console.log(matrixElementsSum([[1, 0, 3], [0, 2, 1], [1, 2, 0]])); // 5
+console.log(matrixElementsSum([[1, 1, 1, 0], [0, 5, 0, 1], [2, 1, 3, 10]])); // 9
+console.log(matrixElementsSum([[1, 1, 1], [2, 2, 2], [3, 3, 3]])); // 18
+console.log(matrixElementsSum([[0]])); // 0
+console.log(matrixElementsSum([[1, 0, 3], [0, 2, 1], [1, 2, 0]])); // 5

b8eff9fa89061c73f28a1aeacae159d3a014a075
Jordan Stoddard <jjsincorporated@gmail.com>
2019-08-12 16:08:23 -0700

    initialize problem

---
+++ CodeSignal/sortByHeight.js
@@ -0,0 +1,19 @@
+// Some people are standing in a row in a park. 
+// There are trees between them which cannot be moved. 
+// Your task is to rearrange the people by their heights in a non-descending order without moving the trees. 
+// People can be very tall!
+
+// Example
+
+// For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
+// sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190].
+
+function sortByHeight(a) {
+
+}
+
+console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180])) // [-1, 150, 160, 170, -1, -1, 180, 190]
+console.log(sortByHeight([-1, -1, -1, -1, -1])) // [-1, -1, -1, -1, -1]
+console.log(sortByHeight([-1])) // [-1]
+console.log(sortByHeight([4, 2, 9, 11, 2, 16])) // [2, 2, 4, 9, 11, 16]
+console.log(sortByHeight([2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1])) 

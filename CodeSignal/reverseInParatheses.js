e31db0c152eda3ec56343a85dc70ea66e1ee1ebf
Jordan Stoddard <jjsincorporated@gmail.com>
2019-08-13 16:31:49 -0700

    continue working on logic

--- CodeSignal/reverseInParatheses.js
+++ CodeSignal/reverseInParatheses.js
@@ -14,14 +14,32 @@
 // reverseInParentheses(inputString) = "foobazrabblim".
 // Because "foo(bar(baz))blim" becomes "foo(barzab)blim" and then "foobazrabblim".
 
-// If 
+
+// If inputString.length === 0 or inputString === "()" return ''
+
+// create a helper function that returns true or false if there's a nested set.
+
+// if nested loop through and count the number of opening parenths.
+// if current letter is ( and 
+
+// Initialize a finalString,
+// open check, closed check, nested check
+// return index
+// loop through inputString
+// if not open concat to final string
+// if ( concat and make open, save return index
+// if open, count until closed
+// if ( and open, 
+
+
+
 
 function reverseInParentheses(inputString) {
-
+    return inputString.split('').reverse().join('')
 }
 
 console.log(reverseInParentheses("(bar)")) // rab
-console.log(reverseInParentheses("foo(bar)baz")) // foorabbaz
-console.log(reverseInParentheses("foo(bar)baz(blim)")) // foorabbazmilb
+// console.log(reverseInParentheses("foo(bar)baz")) // foorabbaz
+// console.log(reverseInParentheses("foo(bar)baz(blim)")) // foorabbazmilb
 console.log(reverseInParentheses("foo(bar(baz))blim")) // foobazrabblim
-console.log(reverseInParentheses("(abc)d(efg)")) // cbadgfe
+// console.log(reverseInParentheses("(abc)d(efg)")) // cbadgfe

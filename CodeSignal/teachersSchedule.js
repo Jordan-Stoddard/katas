1663dd0de9a4b6344313e25b0a75e64682ccad08
Jordan Stoddard <jjsincorporated@gmail.com>
2019-08-22 15:48:23 -0700

    Clarifying comments

--- CodeSignal/teachersSchedule.js
+++ CodeSignal/teachersSchedule.js
@@ -24,23 +24,31 @@ async function suggestSchedule(parentRequest, teacher) {
     */
     const teacherSchedule = await getTeacherSchedule(teacher)
     
+    // Loop through the teachersAvailability object
       for (let day in teacherAvailability) {
+        // save the availableHours array at current day
             let availableHours = teacherAvailability[day]
+            // If we have a parentRequest at the current day in teacherAvailability
             if (parentRequest[day]) {
+              // Loop through the hours in the teacher's available hours.
                 for (let hour of availableHours) {
+                  // If the parentRequest at the current day includes the current hour in the teacher's availability array
                   if (parentRequest[day].includes(hour)) {
+                    // set the start time to be two weeks from that day.
                    let startTime = twoWeeksLater(day, hour)
+                   // set the end time to be one hour after the start time.
                    let endTime = addOneHour(startTime)
-                   
+                   // if the current start time is already in the scheduleObj's start time, continue to the next available match.
                    if (teacherSchedule.some((scheduleObj) => 
                     scheduleObj.start_time * 1 === startTime * 1)) {
                     continue
                    }
+                   // else, return the proposed class schedule.
                    return {start_time: startTime, end_time: endTime}
                   }
                 }
               }
             }
-    
+    // if we make it through this loop without finding any matches, return null.
     return null
   }
